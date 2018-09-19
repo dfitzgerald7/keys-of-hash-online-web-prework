fb8c47c13b1ba4f195e46102b4b573b6c41bf8bf
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-19 16:12:09 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
     my_array = []
     self.each do |key, value|
      binding.pry
-      my_array << key if arguments.include?(key)
+      my_array << key if arguments.include?(value)
     end 
     my_array  
   end
