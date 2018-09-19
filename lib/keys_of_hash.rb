460cbf3d4d0541c1c422145d1e94645a1b803a7d
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-19 16:11:32 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     my_array = []
     self.each do |key, value|
-     # binding.pry
+     binding.pry
       my_array << key if arguments.include?(key)
     end 
     my_array  
